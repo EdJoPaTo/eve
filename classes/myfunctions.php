7d96df13ab0ff887a97e847773734763cce20321
XMAS2014 <edjopato@gmail.com>
2015-03-23 23:25:31 +0100

    eveauth ist im main dir, nicht im aktuellen, wo auch immer man grade ist... brain fail...

--- classes/myfunctions.php
+++ classes/myfunctions.php
@@ -284,7 +284,7 @@
 			$returnString .= "\t\t\t\t".'<a style="background-image: url(//images.contentful.com/idjq7aai9ylm/4fSjj56uD6CYwYyus4KmES/4f6385c91e6de56274d99496e6adebab/EVE_SSO_Login_Buttons_Large_Black.png?w=270&amp;h=45); background-repeat: no-repeat; background-position: center center; min-width: 270px; width: 270px; height: 45px; padding: 0px 10px;" href="'.OAuth::eveSSOLoginURL().'"></a>'."\n";
 		} else {
 			$returnString .= "\t\t\t\t".'<div class="img pilot64" style="padding-left: 75px; background-size: 64px 64px;">'.$_SESSION['characterName']."</div>\n";
-			$returnString .= "\t\t\t\t".'<a href="eveauth.php">Settings</a>'."\n";
+			$returnString .= "\t\t\t\t".'<a href="/eveauth.php">Settings</a>'."\n";
 		}
 /**/
 		$returnString .= "\t\t\t".'</div>'."\n";
